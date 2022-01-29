d7f5373f47efa91199b3dfe4995e4da25f862b51
hthomp15 <hailethomp@gmail.com>
2022-01-29 15:35:09 -0500

    display data, and date for current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,64 @@
-// trying to use a geolocation to get lat/lon coordinates
-// var location = "london";
-// var coordRequest = `http://api.openweathermap.org/geo/1.0/direct?q=London,UK&limit=5&appid=1c6a9b4261903a20ca46df884c067f7b`;
-// fetch(coordRequest).then(function(response){
-//     return console.log(response.json());
+// This will display the current weather on the index.html "current-weather"
+var displayCurrentWeather = (weather_data)=>{
+   
+    var tempEl = document.querySelector(".temp");
+    var windEl = document.querySelector(".wind");
+    var humidEl = document.querySelector(".humid");
+    var uviEl = document.querySelector(".uvi");
+    console.log(weather_data);
+    tempEl.textContent += `${weather_data.current.temp}°F`
+    windEl.textContent += `${weather_data.current.wind_speed}mph`
+    humidEl.textContent += `${weather_data.current.humidity}%`
+    uviEl.textContent += weather_data.current.uvi
     
-// })
+}
+// Uses first fetch data to display city 
+// Uses javascript to display date
+var displayCityAndDate = (data1)=>{
+    var cityEl = document.querySelector(".city")
+        cityEl.textContent = data1.name
+}
 
 
-
-let lon = "40.6782"
-let lat = "73.9442"
-var weatherData = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely&appid=1c6a9b4261903a20ca46df884c067f7b`
-
-fetch(weatherData).then(function(response) {
-    return console.log(response.json());
+// This will display the 5 day forecast
+var displayExtendedForcast = (future_data)=>{
     
-})
+}
+
+
+
+// Gets my weather by city name 
+var getWeather = ()=> {
+    var api_key = '1c6a9b4261903a20ca46df884c067f7b'
+    var city_name = 'london'
+    var weatherData = `http://api.openweathermap.org/data/2.5/weather?appid=${api_key}&q=${city_name}&units=imperial`
+    fetch(weatherData).then(function(response) {
+         return response.json()
+    }).then(function(data) {
+        console.log(data)
+        var lat = data.coord.lat
+        var lon = data.coord.lon
+        displayCityAndDate(data)
+        getExtendedForcast(lat, lon)
+        
+    })
+};
+
+
+
+getWeather()
+// gets an extended 5 day forecast of a specified location. 
+// use lat and lon of city name fetch as inputs. 
+var getExtendedForcast = (lat, lon)=> {
+    var api_key = '1c6a9b4261903a20ca46df884c067f7b'
+    var extendedData = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutes&exclude=alerts&appid=${api_key}&units=imperial`
+
+    fetch(extendedData).then(function(response) {
+        return response.json();
+    }).then(function(data) {
+        console.log(data);
+        displayCurrentWeather(data);
+        
+    })
+}
+
